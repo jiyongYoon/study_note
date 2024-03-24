473a6b83ec4021709471030d15e36a263176954e
jiyong.yoon <jyyoon0615@naver.com>
2024-03-24 22:26:29 +0900

    스레드 생성2

---
+++ src/main/java/java_spring/thread/CreateThread2.java
@@ -0,0 +1,22 @@
+package java_spring.thread;
+
+public class CreateThread2 {
+
+    public static void main(String[] args) {
+        Thread thread = new NewThread();
+        thread.start();
+        /**
+         * My name is Thread[Thread-0,5,main]
+         * My name is Thread-0
+         */
+    }
+
+    private static class NewThread extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("My name is " + Thread.currentThread());
+            System.out.println("My name is " + this.getName());
+        }
+    }
+}
