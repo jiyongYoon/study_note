13a265e39bf04d24328692a2127bcc3677aa3762
jiyong.yoon <jyyoon0615@naver.com>
2024-03-25 22:20:12 +0900

    스레드 생성 실습

---
+++ src/main/java/java_spring/thread/MultiExecutor.java
@@ -0,0 +1,31 @@
+package java_spring.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiExecutor {
+
+    List<Thread> threadList = new ArrayList<>();
+
+    public MultiExecutor(List<Runnable> tasks) {
+        for (Runnable task : tasks) {
+            Thread thread = new Thread(task);
+            threadList.add(thread);
+        }
+    }
+
+    public void executeAll() {
+        this.threadList.forEach(Thread::start);
+    }
+
+    public static void main(String[] args) {
+        List<Runnable> tasks = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int value = i;
+            tasks.add(() -> System.out.println("hello! I'm " + value));
+        }
+        MultiExecutor multiExecutor = new MultiExecutor(tasks);
+        multiExecutor.executeAll();
+    }
+
+}
