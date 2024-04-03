527054ed0b1520d37aea92064a36c5829b238c3b
jiyong.yoon <jyyoon0615@naver.com>
2024-04-03 21:46:18 +0900

    스레드 종료

---
+++ src/main/java/java_spring/thread/InterruptTest.java
@@ -0,0 +1,116 @@
+package java_spring.thread;
+
+import java.math.BigInteger;
+
+public class InterruptTest {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BlockingTask());
+
+        thread.start(); // 스레드가 실행되어 sleep 상태였는데
+        thread.interrupt(); // interrupt 되어 Exception 발생 및 처리
+        /**
+         * block!
+         * exiting sleep thread
+         */
+
+
+        Thread thread1 = new Thread(new LongComputationTask(
+            new BigInteger("200000"), new BigInteger("1000000000")
+        ));
+
+        thread1.start(); // 스레드가 실행되어 큰 계산 중이였는데
+        thread1.interrupt(); // interrupt 되어 작업 마무리
+        /**
+         * you are interrupted!! exit now!
+         * 200000^1000000000 = 0
+         */
+
+        Thread thread2 = new Thread(new NoInterruptLongComputationTask(
+            new BigInteger("200000"), new BigInteger("10000000")
+        ));
+
+        thread2.setDaemon(true); // 해당 스레드를 Daemon으로 돌리면 main스레드에서 더이상 관여하지 않는다.
+        thread2.start();
+        thread2.interrupt(); // 따라서 interrupt 여부와 상관없이 main thread가 종료되면 앱이 종료된다.
+
+        System.out.println("main thread complete");
+        /**
+         * block!
+         * exiting sleep thread
+         * you are interrupted!! exit now!
+         * main thread complete
+         * 200000^1000000000 = 0
+         */
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("block!");
+            try {
+                Thread.sleep(200000); // interrupt가 발생하면 InterruptedException 발생
+            } catch (InterruptedException e) {
+                System.out.println("exiting sleep thread");
+            }
+        }
+    }
+
+    private static class LongComputationTask implements Runnable {
+        private final BigInteger base;
+        private final BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(base + "^" + power + " = " + pow(base, power));
+        }
+        
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                result = result.multiply(base);
+                // 이 경우에는 thread.sleep 처럼 InterruptedException이 발생하지 않기 때문에
+                // 명시적으로 긴 작업중에 interrupt를 받을 수 있도록 처리해주어야 함
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("you are interrupted!! exit now!");
+                    return BigInteger.ZERO;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    private static class NoInterruptLongComputationTask implements Runnable {
+        private final BigInteger base;
+        private final BigInteger power;
+
+        public NoInterruptLongComputationTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(base + "^" + power + " = " + pow(base, power));
+        }
+
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                result = result.multiply(base);
+            }
+
+            return result;
+        }
+    }
+
+}
--- src/main/java/java_spring/thread/README.md
+++ src/main/java/java_spring/thread/README.md
@@ -17,3 +17,25 @@
 <img src="https://github.com/jiyongYoon/study_note/assets/98104603/7031a412-a4d5-44ed-8442-5592f753648a" alt="adder" width="60%" />
 
 - Thread 내에서 Exception을 직접 처리할수도 있다.
+
+## Thread의 종료
+
+### Thread를 종료해야하는 이유
+
+1. 아무것도 하지 않고있어도, 스레드가 살아있다면 리소스를 잡아먹는다. (메모리와 커널, 만약 동작한다면 CPU까지 점유)
+2. 예기치 못하게 잘못된 동작을 계속 반복하거나, 너무 긴 작업을 하고 있거나 등, 개발자의 의도에 벗어나는 동작을 하는 경우
+3. 프로그램을 종료하고 싶은 경우 (스레드가 한 개라도 작업중이라면 당장 프로세스가 종료되지 못한다)
+
+### Thread를 종료하는 방법
+
+`thread.interrupt()`
+
+- A 스레드에서 B 스레드의 interrupt() 메서드를 호출하면, B 스레드에 interrupt가 발생한다.
+  - 이 때, 두가지 상황이 생긴다.
+    1. B 스레드가 `Thread.sleep()` 등의 상황에 멈취있는 등 `try - catch` 블럭에서 `InterruptedException`을 처리하는 경우
+       - 이 경우에는 interrupt 발생 시 `InterruptedException` 가 발생하여 catch 블럭에서 핸들링한 대로 처리된다.
+    2. B 스레드가 정상적인 작업중인 경우
+       - 이 경우에는 interrupt가 발생한다고 해당 스레드가 영향을 받지는 않는다. 따라서 예시 코드와 같이 긴 작업을 하는 경우, 
+       interrupt가 발생하면 멈출 수 있도록 처리를 해줄 수 있다.
+
+       <img src="https://github.com/jiyongYoon/study_note/assets/98104603/737f5d23-9b13-4d03-ae2e-fe26037a469e" alt="adder" width="60%" />
